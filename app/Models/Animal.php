2890919f84648510fd53bfbe0733422c4bd1ebee
Daniel Tran <danieltran354@gmail.com>
2025-12-03 10:44:02 +0100

    Newly registered user automatically gets a new animal added to their collection. Fluffy the fox

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     //
-    
+    protected $table = 'animals';
 
+    public $timestamps = false;
+    protected $fillable = ['user_id', 'name', 'happiness', 'hunger', 'cleanliness', 'species_tag', 'adopted_at', 'updated_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
