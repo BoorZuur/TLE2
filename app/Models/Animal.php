82476b9819be84b48464349875480a906db13b36
jeff9912 <1095143@hr.nl>
2025-12-03 10:29:58 +0100

    fist commit

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Animal extends Model
 {
-    //
-    
+    protected $fillable = ['user_id', 'name', 'hunger', 'cleanliness'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
