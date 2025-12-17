934b29a0d05ea83fdeb43a9443d60fdd1709d6c3
Daniel Tran <danieltran354@gmail.com>
2025-12-17 13:22:16 +0100

    Gebruikers gaan nu direct naar dieren overzicht pagina na inloggen/session actief

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Animal;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
         if (auth()->check()) {
-            $animal = Animal::first();
+            $user = Auth::user();
 
-            return view('home', compact('animal'));
+            $animals = $user->animals()
+                ->with('species')
+                ->orderBy('adopted_at', 'desc')
+                ->get();
+
+            return view('animals.index', compact('animals'));
         } else {
             return view('index');
         }
